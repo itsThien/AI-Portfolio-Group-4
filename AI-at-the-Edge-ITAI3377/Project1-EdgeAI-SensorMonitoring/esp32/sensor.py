6b111ae40f34e9fb4855038c1442b0ae3aa26f36
Thien Nguyen <124387043+itsThien@users.noreply.github.com>
2025-12-07 20:18:15 -0600

    Delete AI-at-the-Edge-ITAI3377 directory

--- AI-at-the-Edge-ITAI3377/Project1-EdgeAI-SensorMonitoring/esp32/sensor.py
+++
@@ -1,8 +0,0 @@
-import random
-
-class SensorReader:
-    def read(self):
-        temp = round(random.uniform(20,30),1)
-        hum = round(random.uniform(40,60),1)
-        vibration = round(random.uniform(0,0.05),3)
-        return [temp, hum, vibration]
